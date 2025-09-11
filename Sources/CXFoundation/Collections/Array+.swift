1e2283a6086c6e88b5ab9d2edcee2f366758a8ad
Cunqi Xiao <Cunqi@users.noreply.github.com>
2025-09-10 18:22:13 -0700

    [CXFoundation] Add Array+

---
+++ Sources/CXFoundation/Collections/Array+.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  CXFoundation
+//
+//  Created by Cunqi Xiao on 9/10/25.
+//
+
+import Foundation
+
+extension Array {
+    public var isNotEmpty: Bool {
+        !isEmpty
+    }
+}
